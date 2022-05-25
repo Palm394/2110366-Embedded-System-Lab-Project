6adf63641ed518dc5246e6d60b2a5ee6b3d33d19
Palm <palm.nichapat@gmail.com>
2022-05-25 22:20:56 +0700

    refactor function

--- hardware/nodemcu/nodemcu.ino
+++ hardware/nodemcu/nodemcu.ino
@@ -1,28 +1,48 @@
 #include <FirebaseESP8266.h>
 #include <ESP8266WiFi.h>
 
-#define WIFI_SSID "<WIFI_NAME>";
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+#define WIFI_SSID "<WIFI_NAME>"
 #define WIFI_PASSWORD "<WIFI_PASSWORD>"
 
 #define FIREBASE_HOST "limit-people-19e80-default-rtdb.asia-southeast1.firebasedatabase.app"
 #define FIREBASE_KEY "u5CwTtfZPrItD0pzhyTSAM0cj5DO1pbrnFe1p7Ar"
 
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 FirebaseData firebaseData;
 
 void setup() {
     connectWifi();
     Firebase.begin(FIREBASE_HOST, FIREBASE_KEY);
-    //recieve from stm32
-    //int recieved_data = ......;
+    mlx.begin();
+}
+
+void loop() {
+
+}
+
+void callTemp(){
+    Serial.print("Ambient = "); 
+    Serial.print(mlx.readAmbientTempC()); 
+    Serial.print("*C\tObject = "); 
+    Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+    Serial.print("Ambient = "); 
+    Serial.print(mlx.readAmbientTempF()); 
+    Serial.print("*F\tObject = "); 
+    Serial.print(mlx.readObjectTempF()); Serial.println("*F");
+
+    Serial.println();
+    delay(500);
+}
+
+void postValueTofirebase(int val){
     if(Firebase.setInt(firebaseData, "/count", 6)) {
         Serial.println("Added");
     } else {
         Serial.println("Error : " + firebaseData.errorReason());
     }
-}
-
-void loop() {
-
 }
 
 void connectWifi() {
