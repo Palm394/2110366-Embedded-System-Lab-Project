42ee7b91e4830d68aa0c1ac382dbb6d82a862fe1
Palm <palm.nichapat@gmail.com>
2022-05-27 11:46:38 +0700

    rerender hidden state

--- frontend/index.js
+++ frontend/index.js
@@ -20,17 +20,20 @@ onValue(numCountRef, (snapshot) => {
   const data = parseInt(snapshot.val());
   const limitNum = 5; // change limit number here
   if (data > limitNum) { 
+    document.getElementById("Exceed").hidden = false;
     document.getElementById("Exceed").innerHTML = "Exceed Limit Number";
     document.getElementById("Full").hidden = true;
     document.getElementById("Number").hidden = true;
   }
   else if (data == limitNum) {
     document.getElementById("Exceed").hidden = true;
+    document.getElementById("Full").hidden = false;
     document.getElementById("Full").innerHTML = "Room Full";
     document.getElementById("Number").hidden = true;
   }
   else {
     document.getElementById("Exceed").hidden = true;
+    document.getElementById("Number").hidden = false;
     document.getElementById("Number").innerHTML = data;
     document.getElementById("Full").hidden = true;
   }
